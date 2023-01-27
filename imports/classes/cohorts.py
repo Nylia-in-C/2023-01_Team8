a6b09fa3491f06d76218b67735c735dbefecad84
Brant-Harker <brant.harker12@gmail.com>
2023-01-26 17:58:49 -0700

    cohort class as a dataclass

--- imports/classes/cohorts.py
+++ imports/classes/cohorts.py
@@ -9,22 +9,25 @@
 # To initialize: pass program ID, term ID, student count (cohort ID increments by 1),
 # Name is created by concatenating these
 
+from dataclasses import dataclass
+
+# Count of cohorts for autoincrement in class
+cohortCounts = {"PM01" : 0, "PM02" : 0, "PM03" : 0,
+                "BA01" : 0, "BA02" : 0, "BA03" : 0,
+                "GLM01": 0, "GLM02": 0, "GLM03": 0, 
+                "FS01" : 0, "FS02" : 0, "FS03" : 0, 
+                "DXD01": 0, "DXD02": 0, "DXD03": 0,
+                "BK01" : 0, "BK02" : 0, "BK03" : 0 }
+
+@dataclass
 class Cohort:
+    programID: str
+    termID: str
+    count: int
 
-    cohortCounts = {"PM01" :  0, "PM02" :  0, "PM03" :  0,
-                    "BA01" :  0, "BA02" :  0, "BA03" :  0,
-                    "GLM01" : 0, "GLM02" : 0, "GLM03" : 0, 
-                    "FS01" :  0, "FS02" :  0, "FS03" :  0, 
-                    "DXD01" : 0, "DXD02" : 0, "DXD03" : 0,
-                    "BK01" :  0, "BK02" :  0, "BK03" :  0}
-    #Abbreviation for BookKeeping Certificate is not in specs, using "BK" for now
-
-    def __init__(self, programID, termID, studentCount):
+    def __post_init__(self):
         #Increment cohorts
-        Cohort.cohortCounts[programID + termID] += 1
+        cohortCounts[self.programID + self.termID] += 1
+        self.cohortID = "{:02d}".format(cohortCounts[self.programID + self.termID])
 
-        self.programID = programID
-        self.termID = termID
-        self.cohortID = "{:02d}".format(Cohort.cohortCounts[programID + termID])
         self.name = self.programID + self.termID + self.cohortID
-        self.count = studentCount
