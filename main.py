71e22f42448d012a24594706451010ab2c856d7c
Ken <kennethlieu@gmail.com>
2023-01-28 20:47:39 -0700

    Fixed base layout. Added File selection. Can run main

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 #main python file - 395 team 8
+
+from UI import UI
+from PyQt5.QtWidgets import *
+def main():
+    app = QApplication([])
+    window = UI.UI()
+    window.show()
+    app.exec_()
+
+
+if __name__ == "__main__":
+    main()
