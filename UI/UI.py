a1fd1035628cdff7c0f1ac9b425e419276c95bf0
Ken <kennethlieu@gmail.com>
2023-01-30 13:19:16 -0700

    Added efficiency input section. Made functions to clear out table. Table cannot be editted

--- UI/UI.py
+++ UI/UI.py
@@ -22,6 +22,9 @@ class UI(QMainWindow):
         self.file_path = ""
         self.file_label = QLabel()
         self.table = QTableWidget()
+        # Make table unedittable
+        self.table.setEditTriggers(QAbstractItemView.EditTrigger.NoEditTriggers)
+        self.efficiency_input = QLineEdit()
 
         self.setWindowTitle("Scheduler")
 
@@ -38,15 +41,16 @@ class UI(QMainWindow):
     def create_vlayout(self):
         vbox = QVBoxLayout(self)
         top_hbox = self.create_topHlayout()
-        bottom_hbox = self.create_botHlayout()
+        bottom_hbox = self.create_botlayout()
 
         # Add layouts to overall area
         vbox.addLayout(top_hbox)
+        vbox.addSpacing(100)
         vbox.addLayout(bottom_hbox)
+        vbox.addSpacing(20)
 
         # Add spacing, currently there are "4" items (tophbox, bothbox, spacer 1, spacer 2)
-        vbox.insertSpacing(1, 100)
-        vbox.insertSpacing(3, 70)
+
         return vbox
 
     # Creates the top hbox where most information will be displayed
@@ -57,6 +61,84 @@ class UI(QMainWindow):
         # Add stuff into this HBox as needed
         # This will most likely be the display of most information
         return hbox
+
+    # Creates the bottom layout where most user interaction takes place
+    def create_botlayout(self):
+
+        vbox = QVBoxLayout(self)
+
+        vbox.addLayout(self.create_file_choose())
+
+        vbox.addSpacing(50)
+
+        vbox.addLayout(self.create_class_efficiency())
+
+        return vbox
+
+    # create the file choose layout / widgets
+    def create_file_choose(self):
+
+        hbox_file_choose = QHBoxLayout(self)
+        choose_input_button = QPushButton("Choose File", self)
+        choose_input_button.setMaximumWidth(100)
+        choose_input_button.clicked.connect(self.choose_file)
+
+        self.file_label.setText("No File Chosen")
+        self.file_label.setFont(QFont("Arial", 14))
+
+        hbox_file_choose.addWidget(choose_input_button)
+        hbox_file_choose.addWidget(self.file_label)
+        return hbox_file_choose
+
+    # Makes layout for the class efficiency section
+    def create_class_efficiency(self):
+        hbox_all = QHBoxLayout()
+        vbox = QVBoxLayout()
+        hbox_info = QHBoxLayout()
+
+        label = QLabel("Proposed Cohort Size:")
+        label.setFont(QFont("Times", 10))
+        label.setMaximumWidth(150)
+
+        self.efficiency_input= QLineEdit()
+        self.efficiency_input.setPlaceholderText("Eg. 5")
+
+        calculate = QPushButton("Calculate Efficiency")
+        # Need to connect this button to a function
+
+        hbox_info.addWidget(label)
+        hbox_info.addWidget(self.efficiency_input)
+
+        vbox.addLayout(hbox_info)
+        vbox.addWidget(calculate)
+
+        hbox_all.addLayout(vbox)
+
+        # Remove this spacing if adding more stuff
+        hbox_all.insertSpacing(1, self.width())
+
+        return hbox_all
+
+
+
+
+    # Action event for the choose file button
+    def choose_file(self):
+
+        chosen_file = QFileDialog.getOpenFileName(
+            self,
+            "Choose a file",
+            os.getcwd(),
+            "",  # Filters to be added later to show only excel type files
+            ""
+        )
+        if chosen_file[0] == "":
+            self.file_label.setText("No File Chosen")
+            self.file_path = ""
+        else:
+            self.file_path = chosen_file[0]
+            self.file_label.setText(chosen_file[0])
+
 
     # Settings to create the TableView
     def create_tableview(self):
@@ -80,6 +162,10 @@ class UI(QMainWindow):
         except:
             print("Could not read database")
 
+
+    # The following section will be for action events
+    # or functions that are called repeatedly
+    # after the initial startup
     def create_column_headers(self, cursor_obj):
         table_columns.clear()
         all_columns = cursor_obj.description
@@ -88,6 +174,7 @@ class UI(QMainWindow):
 
         self.table.setColumnCount(len(table_columns))
         self.table.setHorizontalHeaderLabels(table_columns)
+        self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
 
     # Load data into tableview
     def load_data(self, row_data):
@@ -103,50 +190,6 @@ class UI(QMainWindow):
         self.table.clear()
         self.table.setColumnCount(0)
         self.table.setRowCount(0)
-
-
-    # Creates the bottom hbox where most user interaction takes place
-    def create_botHlayout(self):
-        hbox = QHBoxLayout(self)
-
-        self.create_file_label()
-
-        hbox.addWidget(self.create_buttons())
-        hbox.addWidget(self.file_label)
-        # Add stuff into this HBox as needed
-        # Maybe make it inside a vbox later?
-        return hbox
-
-    # Currently only creates choose input button, can add more when / if needed
-    def create_buttons(self):
-        choose_input_button = QPushButton("Choose File", self)
-        choose_input_button.setMaximumWidth(100)
-        choose_input_button.clicked.connect(self.choose_file)
-        return choose_input_button
-
-    # The settings for the file chosen label
-    def create_file_label(self):
-
-        self.file_label.setText("No File Chosen")
-        self.file_label.setFont(QFont("Arial", 14))
-
-    # Action event for the choose file button
-    def choose_file(self):
-
-        chosen_file = QFileDialog.getOpenFileName(
-            self,
-            "Choose a file",
-            os.getcwd(),
-            "",  # Filters to be added later to show only excel type files
-            ""
-        )
-        if chosen_file[0] == "":
-            self.file_label.setText("No File Chosen")
-            self.file_path = ""
-        else:
-            self.file_path = chosen_file[0]
-            self.file_label.setText(chosen_file[0])
-
 
 
 # total cohort size:
