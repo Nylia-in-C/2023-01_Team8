872a73d78ec8c83193a92473a851f24606f07651
Ken <kennethlieu@gmail.com>
2023-01-31 14:31:24 -0700

    Functionality to cohort calcs. Shows in tableview.

--- UI/UI.py
+++ UI/UI.py
@@ -1,16 +1,18 @@
 # main python file - 395 team 8
 
-
 import os
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
 import sqlite3
 
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from imports.create_cohorts import *
 
 table_columns = []
 classrooms = ["11-533", "11-534", "11-560", "11-562",
               "11-564", "11-458", "11-430", "11-320"
               "11-532 (Lab)"]
+
+
 
 class UI(QMainWindow):
 
@@ -24,7 +26,7 @@ class UI(QMainWindow):
         self.table = QTableWidget()
         # Make table unedittable
         self.table.setEditTriggers(QAbstractItemView.EditTrigger.NoEditTriggers)
-        self.efficiency_input = QLineEdit()
+        self.cohort_size = QLabel()
 
         self.setWindowTitle("Scheduler")
 
@@ -71,7 +73,7 @@ class UI(QMainWindow):
 
         vbox.addSpacing(50)
 
-        vbox.addLayout(self.create_class_efficiency())
+        vbox.addLayout(self.create_cohort_size())
 
         return vbox
 
@@ -91,23 +93,25 @@ class UI(QMainWindow):
         return hbox_file_choose
 
     # Makes layout for the class efficiency section
-    def create_class_efficiency(self):
+    def create_cohort_size(self):
         hbox_all = QHBoxLayout()
         vbox = QVBoxLayout()
         hbox_info = QHBoxLayout()
 
-        label = QLabel("Proposed Cohort Size:")
+        label = QLabel("Optimal Cohort Size:")
         label.setFont(QFont("Times", 10))
         label.setMaximumWidth(150)
 
-        self.efficiency_input= QLineEdit()
-        self.efficiency_input.setPlaceholderText("Eg. 5")
+        self.cohort_size.setFont(QFont("Times", 10))
+        self.cohort_size.setMaximumWidth(100)
 
-        calculate = QPushButton("Calculate Efficiency")
+
+        calculate = QPushButton("Calculate Optimal Cohort Size")
+        calculate.clicked.connect(self.load_optimal_cohorts)
         # Need to connect this button to a function
 
         hbox_info.addWidget(label)
-        hbox_info.addWidget(self.efficiency_input)
+        hbox_info.addWidget(self.cohort_size)
 
         vbox.addLayout(hbox_info)
         vbox.addWidget(calculate)
@@ -151,10 +155,10 @@ class UI(QMainWindow):
             # Move to cohort table
             cursor.execute("SELECT * FROM COHORT")
 
-            self.create_column_headers(cursor)
+            self.create_db_column_headers(cursor)
 
             value_fill = cursor.fetchall()
-            self.load_data(value_fill)
+            self.load_db(value_fill)
 
             cursor.close()
             database.close()
@@ -166,7 +170,7 @@ class UI(QMainWindow):
     # The following section will be for action events
     # or functions that are called repeatedly
     # after the initial startup
-    def create_column_headers(self, cursor_obj):
+    def create_db_column_headers(self, cursor_obj):
         table_columns.clear()
         all_columns = cursor_obj.description
         for column_name in all_columns:
@@ -177,7 +181,7 @@ class UI(QMainWindow):
         self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
 
     # Load data into tableview
-    def load_data(self, row_data):
+    def load_db(self, row_data):
 
         self.table.setRowCount(len(row_data))
         columns = self.table.columnCount()
@@ -192,6 +196,60 @@ class UI(QMainWindow):
         self.table.setRowCount(0)
 
 
-# total cohort size:
-# total number of students:
-# Cohorts per program:
+    def load_optimal_cohorts(self):
+
+        self.clear_table()
+
+        # This is a simulated event so far
+
+        # Creates the randomized student number
+        # and randomizes how many students are in programs
+        program_count = random_students()
+        opt_size = get_optimal_cohort_size(program_count, CLASSROOMS)
+        self.cohort_size.setText(str(opt_size))
+
+        cohort_dict = create_cohort_dict(program_count, opt_size)
+
+        # Adding everything to tableview
+
+        # Create Columns
+        table_columns.clear()
+        for programs in program_count.keys():
+            table_columns.append(programs)
+
+        self.table.setColumnCount(len(table_columns))
+        self.table.setHorizontalHeaderLabels(table_columns)
+
+        # Create Rows
+        table_rows = ["Students in Program", "Amount of Cohorts", "Cohort sizes", ""]
+        all_cohorts = []
+        for program in cohort_dict.keys():
+            for cohort_size in cohort_dict[program]:
+                new_cohort = Cohort(program, TERM_ID, cohort_size)
+                table_rows.append(new_cohort.name)
+                all_cohorts.append(new_cohort)
+
+        self.table.setRowCount(len(table_rows))
+        self.table.setVerticalHeaderLabels(table_rows)
+
+        # Enter students in program / amount of cohorts / cohort sizes
+        for row in range(3):
+            for program in range(len(table_columns)):
+                match row:
+                    case 0:
+                        self.table.setItem(row, program, QTableWidgetItem(str(program_count[table_columns[program]])))
+                    case 1:
+                        self.table.setItem(row, program, QTableWidgetItem(str(len(cohort_dict[table_columns[program]]))))
+                    case 2:
+                        self.table.setItem(row, program, QTableWidgetItem(str(cohort_dict[table_columns[program]])))
+
+
+        # Total Students
+        self.table.setItem(3, 0, QTableWidgetItem("Total Students: " + str(sum(program_count.values()))))
+
+        # Enters cohort sizes for each course
+        co_size = 0
+        for each_program in range(4, len(table_rows)):
+            self.table.setItem(each_program, 0, QTableWidgetItem("Cohort size: " + str(all_cohorts[co_size].count)))
+            co_size+=1
+
