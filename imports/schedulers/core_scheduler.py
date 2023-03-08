9b4a08cacf703dbebcefcbfd00ff7f7c8dec4c7d
Brant-Harker <brant.harker12@gmail.com>
2023-03-08 00:12:17 -0700

    Comment out my custom imports

--- imports/schedulers/core_scheduler.py
+++ imports/schedulers/core_scheduler.py
@@ -1,8 +1,8 @@
-import os, sys
-currentdir = os.path.dirname(os.path.realpath(__file__))
-parentdir = os.path.dirname(currentdir)
-grandparentdir = os.path.dirname(parentdir)
-sys.path.append(grandparentdir)
+# import os, sys
+# currentdir = os.path.dirname(os.path.realpath(__file__))
+# parentdir = os.path.dirname(currentdir)
+# grandparentdir = os.path.dirname(parentdir)
+# sys.path.append(grandparentdir)
 
 import math
 import datetime
