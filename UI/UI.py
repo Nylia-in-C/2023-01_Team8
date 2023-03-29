99ca51bffaea19ad01563c66139e3a381192b624
Nylia-in-C <114269785+Nylia-in-C@users.noreply.github.com>
2023-03-29 00:10:05 -0600

    updated UI.py to use check_path()

--- UI/UI.py
+++ UI/UI.py
@@ -297,14 +297,7 @@ class UI(QMainWindow):
 
         # Read me Doc
         read_me = QTextEdit()
-        rdme = '..\\README.md'
-
-        #Check if running in .exe
-        if getattr(sys, 'frozen', False):
-            app_path = os.path.dirname(sys.executable)
-        elif __file__:
-            app_path = os.path.dirname(__file__)
-        rdme_path = os.path.join(app_path, rdme)
+        rdme_path = helpers.check_path('README.md')
 
         file_content = open(rdme_path).read()
         read_me.setMarkdown(file_content)
