95eb98b38434e23d6f7d7c668b06def15fec310f
Brant-Harker <brant.harker12@gmail.com>
2023-03-29 12:41:10 -0400

    Delete old test

--- imports/classes/courses.py
+++ imports/classes/courses.py
@@ -103,11 +103,3 @@ class Lecture(Course):
                 i = i-1
         return ( self.ID, self.title, self.cohort, self.room, self.termHours, self.term, self.duration, self.startWeek, self.startDay, self.startTime, self.isCore, self.isOnline, self.hasLab, preReqsString )
 
-
-#testing purposes
-
-# Dummy = Course('CMSK 1053', 'theTitle', 40, 45, 0,1,0, ["CMSK 1052", "CMSK 0157"])
-# Dummy.printCourse()
-# subDummy = Section('CMSK 1053', 'theTitle', 40, 45, 0,1,0, ["CMSK 1052", "CMSK 0157"])
-# print(subDummy)
-
