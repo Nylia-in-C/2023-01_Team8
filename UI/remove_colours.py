195bdba06e27e34995e23c2bd1e8722373b34cd4
Nylia-in-C <nyliadigital@gmail.com>
2023-03-31 20:30:58 -0600

    took remove_colours function out of UI.py

---
+++ UI/remove_colours.py
@@ -0,0 +1,24 @@
+#Imports
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from openpyxl.reader.excel import load_workbook
+from database.database import *
+
+def remove_colours(BG_COLOURS):
+    excludedcolours = ["aliceblue", "mediumturquoise", "midnightblue", 
+                    "lavenderblush", "blue", "mediumblue", "blanchedalmond", 
+                    "indigo", "seashell", "navy", "black", "brown", "beige",
+                    "azure", "deeppink", "fuchsia", "hotpink", "magenta",
+                    "red", "pink", "mediumvioletred", "blueviolet", "darkviolet",
+                    "mediumpurple", "purple"]
+
+    for colour in excludedcolours:
+        BG_COLOURS.remove(colour)
+
+    _colours = BG_COLOURS.copy()
+    for colour in range(len(_colours)):
+        if "dark" in _colours[colour] or "white" in _colours[colour] or "gray" in _colours[colour] or "grey" in _colours[colour]:
+            BG_COLOURS.remove(_colours[colour])
+
+    return(BG_COLOURS)
